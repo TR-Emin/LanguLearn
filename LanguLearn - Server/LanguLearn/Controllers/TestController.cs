48d7cbc6a1cac2752080b9ef1c82f9ec76d5b8b7
Muhammet Emin Aslangil <eminaslangil@hotmail.com>
2022-08-03 15:44:15 +0300

    added test function in TestController

--- LanguLearn - Server/LanguLearn/Controllers/TestController.cs
+++ LanguLearn - Server/LanguLearn/Controllers/TestController.cs
@@ -1,15 +1,29 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using LanguLearn.Infrastructure.Model;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace LanguLearn.Controllers
 {
-    [Route("api/[controller]")]
     [ApiController]
+    [Route("api/[controller]")]    
     public class TestController : ControllerBase
     {
         public TestController()
         {
 
+        }
+
+        [HttpGet]
+        [Route("TestFunc")]
+        public Test TestFunc()
+        {
+            Test test = new Test
+            {
+                Id = 1,
+                Name = "TestData",
+            };
+
+            return test;
         }
     }
 }
--- LanguLearn - Server/LanguLearn/Infrastructure/Model/Test.cs
+++ LanguLearn - Server/LanguLearn/Infrastructure/Model/Test.cs
@@ -3,5 +3,6 @@
     public class Test
     {
         public int Id { get; set; }
+        public string Name { get; set; }
     }
 }
